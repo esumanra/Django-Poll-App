74ba3f52cbc1f31be8cb886db27a65d65ed7d842
Manikanta Raju <manikantaraju.namburi@bcgtools.com>
2019-08-12 14:05:49 +0530

    initial commit

---
+++ polls/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
